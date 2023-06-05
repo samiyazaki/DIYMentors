2df22a3edf0b9a662724a3d784fcf124afaea942
Seiya Miyazaki <68964442+samiyazaki@users.noreply.github.com>
2023-06-05 16:04:21 -0400

    Adding Component organization

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Navbar = () => {
+    return(
+        <nav>
+            <h1>DIY Pro</h1>
+            <ul>
+                <li><a href = "/">Home</a></li>
+            </ul>
+        </nav>
+    )
+}
