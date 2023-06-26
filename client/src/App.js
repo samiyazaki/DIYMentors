f0f8a237f2a4f33b132f7cded23209cca5b6657f
Seiya Miyazaki <68964442+samiyazaki@users.noreply.github.com>
2023-06-26 14:53:36 -0400

    Adding in home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
-
-import './App.css';
+import Home from './components/Home';
+// import other pages here...
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Navbar />
-        {/* Other components and routes go here */}
+        <Route path="/" exact component={Home} />
+        {/* other routes here... */}
       </div>
     </Router>
   );
---
+++ client/src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+const Home = () => {
+  return (
+    <div className="home">
+      <Navbar />
+      <h1>Welcome to DIY Pro</h1>
+      <p>Explore and create your own DIY projects with our community.</p>
+    </div>
+  );
+};
+
+export default Home;
